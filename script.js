3c0735e1935891c0632ec88f542aa1f897a14bcc
Shoxrux <husanbayevshohruh@gmail.com>
2023-06-09 16:07:26 +0500

    created init

---
+++ script.js
@@ -0,0 +1,26 @@
+// open_signUp_popUp = document.querySelector('.signPopUp')
+// signUp_popUp = document.getElementById('signup')
+// close_signUp_popUp = document.getElementById('button1')
+// console.log(open_signUp_popUp)
+// open_signUp_popUp.addEventListener('click', function(e){
+//     e.preventDefault();
+//     sign_popUp.classList.add('active');
+// })
+
+// close_signUp_popUp.addEventListener('click', () => {
+//     sign_popUp.classList.remove('active');
+// })
+
+
+// open_logIn_popUp = document.getElementById('purpleFont')
+// logIn_popUp = document.getElementById('login')
+// close_logIn_popUp = document.getElementById('button')
+
+// open_logIn_popUp.addEventListener('click', function(d) {
+//     d.preventDefault();
+//     logIn_popUp.classList.add('active');
+// })
+
+// close_logIn_popUp.addEventListener('click', () => {
+//     logIn_popUp.classList.remove('active');
+// })
